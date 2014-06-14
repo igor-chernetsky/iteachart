abe052bfa7785014ba0475b6c25ec7c1440c75bd
andrewQwer <andrew_qwer@mail.ru>
2014-06-14 19:22:05 +0300

    unused actions removed

--- iteachart/Controllers/HomeController.cs
+++ iteachart/Controllers/HomeController.cs
@@ -22,19 +22,5 @@ namespace iteachart.Controllers
             List<User> users = userService.GetUsersList().ToList();
             return View(users);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
