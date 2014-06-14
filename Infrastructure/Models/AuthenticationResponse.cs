4e78366e17d7d686b5124120a440efd7058b0f17
Alex Turuta <aturuta@servicechannel.com>
2014-06-14 13:46:32 +0300

    Implemented Login method

---
+++ Infrastructure/Models/AuthenticationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class AuthenticationResponse
+    {
+        public string ErrorCode { get; set; }
+        public string Persmission { get; set; }
+        public string SessionId { get; set; }
+    }
+}
---
+++ Infrastructure/Services/ISessionService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Infrastructure.Code;
+using Infrastructure.Exceptions;
+using Infrastructure.Models;
+using Infrastructure.Secure;
+
+namespace Infrastructure.Services
+{
+    public interface ISessionService
+    {
+        void Login(LoginModel model);
+
+        void LogOut();
+
+        bool IsLoggedIn();
+    }
+
+    public class SessionService : ISessionService
+    {
+        readonly ISessionFactory sessionFactory;
+
+        public SessionService(ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public void Login(LoginModel model)
+        {
+            var token = sessionFactory.GetSession(model.UserName, model.Password);
+
+            var session = new SessionUser
+            {
+                AuthorizationToken = token,
+                UserName = model.UserName
+            };
+
+            HttpContext.Current.Session["ITechUseSession"] = session;
+        }
+
+        public void LogOut()
+        {
+            HttpContext.Current.Session["ITechUseSession"] = null;
+        }
+
+        public bool IsLoggedIn()
+        {
+            return HttpContext.Current.Session["ITechUseSession"] != null;
+        }
+    }
+}
